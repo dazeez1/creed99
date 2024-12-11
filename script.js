f9eae506d81dcc9e7eb86cb10b6ae25547a30c33
Azeez Damilare Gbenga <144009932+dazeez1@users.noreply.github.com>
2024-12-12 00:12:43 +0200

    update index.html

--- script.js
+++ script.js
@@ -16,29 +16,3 @@ function updateTime() {
 updateTime();
 // Set interval to update every second
 setInterval(updateTime, 1000);
-
-
-const API_URL = 'http://127.0.0.1:8000/api/save-email/';
-
-document.getElementById('email-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const emailInput = document.getElementById('email-input').value;
-
-    try {
-        const response = await fetch(API_URL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email: emailInput }),
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            alert(result.message);
-        } else {
-            alert("Error: " + JSON.stringify(result));
-        }
-    } catch (error) {
-        alert("An error occurred. Please try again.");
-        console.error(error);
-    }
-});
